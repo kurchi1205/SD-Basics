c22230038f3d66b6ae8d407d96b6f28021f6c68d
Prerana <prerana@hellosivi.com>
2023-10-12 08:09:23 +0530

    v2 sd pipelines

--- pipelines.py
+++ pipelines.py
@@ -1,4 +1,4 @@
-from diffusers import AutoencoderKL, LMSDiscreteScheduler, UNet2DConditionModel
+from diffusers import AutoencoderKL, LMSDiscreteScheduler, UNet2DConditionModel, EulerDiscreteScheduler
 from transformers import CLIPTextModel, CLIPTokenizer, logging
 
 
@@ -19,12 +19,25 @@ def get_pipelines(version: str):
         # The noise scheduler
         scheduler = LMSDiscreteScheduler(beta_start=0.00085, beta_end=0.012, beta_schedule="scaled_linear", num_train_timesteps=1000)
 
-        pipeline = {
-            "vae": vae,
-            "tokenizer": tokenizer,
-            "text_encoder": text_encoder,
-            "unet": unet,
-            "scheduler": scheduler
-        }
+    if version == 'v2':
+        vae = AutoencoderKL.from_pretrained("stabilityai/stable-diffusion-2", subfolder="vae")
+
+        # Load the tokenizer and text encoder to tokenize and encode the text.
+        tokenizer = CLIPTokenizer.from_pretrained("openai/clip-vit-large-patch14")
+        text_encoder = CLIPTextModel.from_pretrained("openai/clip-vit-large-patch14")
+
+        # The UNet model for generating the latents.
+        unet = UNet2DConditionModel.from_pretrained("stabilityai/stable-diffusion-2", subfolder="unet")
+
+        # The noise scheduler
+        scheduler = EulerDiscreteScheduler.from_pretrained("stabilityai/stable-diffusion-2", subfolder="scheduler")
+
+    pipeline = {
+        "vae": vae,
+        "tokenizer": tokenizer,
+        "text_encoder": text_encoder,
+        "unet": unet,
+        "scheduler": scheduler
+    }
 
     return pipeline
