5817f3041893f3e31453cb40290005d3da8e1d5c
Prerana <prerana@hellosivi.com>
2023-10-12 07:20:02 +0530

    helper functions

---
+++ utils.py
@@ -0,0 +1,18 @@
+import torch
+from PIL import Image
+
+def set_timesteps(scheduler, num_inference_steps):
+    scheduler.set_timesteps(num_inference_steps)
+    scheduler.timesteps = scheduler.timesteps.to(torch.float32) # minor fix to ensure MPS compatibility, fixed in diffusers PR 3925
+
+
+def latents_to_pil(latents, vae):
+    # bath of latents -> list of images
+    latents = (1 / 0.18215) * latents
+    with torch.no_grad():
+        image = vae.decode(latents).sample
+    image = (image / 2 + 0.5).clamp(0, 1)
+    image = image.detach().cpu().permute(0, 2, 3, 1).numpy()
+    images = (image * 255).round().astype("uint8")
+    pil_images = [Image.fromarray(image) for image in images]
+    return pil_images
