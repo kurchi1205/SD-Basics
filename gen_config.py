e815cf4d878d1c537baba9a5b173757f31674274
prerana <prerana.chaks@gmail.com>
2023-10-13 18:32:30 +0000

    config for gen

---
+++ gen_config.py
@@ -0,0 +1,35 @@
+import json
+
+config_for_gen = {
+    "anders-zorn": {
+        "color_code": [209, 255, 59],
+        "col_name": 'lemon_green'
+        "seed": 30
+    },
+    
+    "dreamcore": {
+        "color_code": [255, 184, 51],
+        "col_name": 'orange'
+        "seed": 30
+    },
+    
+    "manga": {
+        "color_code": [144, 51, 255],
+        "col_name": 'violet'
+        "seed": 30
+    },
+    
+    "rim-illustration": {
+        "color_code": [153, 0, 115],
+        "col_name": "mauve"
+        "seed": 30
+    },
+    
+    "pjablonski-style" : {
+        "color_code": [204, 51, 0],
+        "col_name": 'brown'
+        "seed": 30
+    },
+}
+        
+        
